385cc806e2a192adb1d7d713bc85648e7e9cc6bb
Jordan Hemmers <jhemmers@hotmail.com>
2020-01-24 01:40:08 -0500

    Camera follows player

---
+++ aMinerIssue/aMinerIssueGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    public float followAhead;
+
+    private Vector3 targetPosition;
+
+    public float smoothing;
+
+    public bool followTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+        followTarget = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget)
+        {
+            targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+            if (target.transform.localScale.x > 0f)
+            {
+                targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+            }
+            else
+            {
+                targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+            }
+
+            //transform.position = targetPosition;
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        }
+    }
+}
