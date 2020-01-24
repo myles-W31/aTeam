453ec4a6b1c3f6838f9f7b10a2ec785fe4af722d
Pancakes42 <36553713+Pancakes42@users.noreply.github.com>
2020-01-23 23:47:29 -0500

    added steve and a spider. steve now moves when you press the left and right arrow keys

--- aMinerIssue/aMinerIssueGame/Assets/Scripts/characterMovement.cs
+++ aMinerIssue/aMinerIssueGame/Assets/Scripts/characterMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class characterMovement : MonoBehaviour
 {
+    BoxCollider2D myCollider;
+    public float speed = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed;
+        }
+
+        transform.position = pos;
     }
 }
