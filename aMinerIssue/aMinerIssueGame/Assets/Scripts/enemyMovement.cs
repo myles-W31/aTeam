a4cc8010890e4c1a61a6f0b5e4e14219ca0490ef
mjw7119 <mjw7119@rit.edu>
2020-01-29 12:28:09 -0500

    Enemy movement is in!!

--- aMinerIssue/aMinerIssueGame/Assets/Scripts/enemyMovement.cs
+++ aMinerIssue/aMinerIssueGame/Assets/Scripts/enemyMovement.cs
@@ -7,12 +7,19 @@ public class enemyMovement : MonoBehaviour
     public GameObject theEnemy;
     public float enemyHealth;
     private float maxEnemyHealth;
+    public GameObject pointA;
+    public GameObject pointB;
+    bool moveRight;
 
+    public Rigidbody2D enemyRigid;
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
         maxEnemyHealth = 1;
         enemyHealth = maxEnemyHealth;
+        moveRight = false;
+        enemyRigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,10 +29,40 @@ public class enemyMovement : MonoBehaviour
         {
             Destroy(theEnemy.gameObject);
         }
+
+        //if moveRight is false, move to point A
+        if (moveRight == false)
+        {
+            enemyRigid.velocity = new Vector3(-moveSpeed, enemyRigid.velocity.y, 0f);
+            transform.localScale = new Vector3(.5f, .5f, .5f);
+        }
+        //check Location
+        turnRight(transform.position, pointA.transform.position, pointB.transform.position);
+        //if moveRight is true, move to point B
+        if (moveRight == true)
+        {
+            enemyRigid.velocity = new Vector3(moveSpeed, enemyRigid.velocity.y, 0f);
+            transform.localScale = new Vector3(-.5f, .5f, .5f);
+        }
     }
 
     public void HurtEnemyMethod(enemyMovement objectToHurt, float damageToTake)
     {
         objectToHurt.enemyHealth -= damageToTake;
+    }
+
+    public void turnRight(Vector3 enemyLocation, Vector3 pointALocation, Vector3 pointBLocation)
+    {
+        //check point A first
+        float check = Vector3.Distance(pointALocation, enemyLocation);
+        if (check <= 2)
+        {
+            moveRight = true;
+        }
+        check = Vector3.Distance(pointBLocation, enemyLocation);
+        if (check <= 2)
+        {
+            moveRight = false;
+        }
     }
 }
