43f2827e2f81a84af1a9171b6fca13594b773677
Jordan Hemmers <jhemmers@hotmail.com>
2020-02-06 15:14:06 -0500

    random level loading

---
+++ aMinerIssue/aMinerIssueGame/Assets/Scripts/LevelGoalScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGoalScript : MonoBehaviour
+{
+
+    public string levelToLoad1;
+    public string levelToLoad2;
+    public string levelToLoad3;
+    public string levelToLoad4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            int index = Random.Range(1, 5);
+            if(index == 1)
+            {
+                SceneManager.LoadScene(levelToLoad1);
+            }
+            else if (index == 2)
+            {
+                SceneManager.LoadScene(levelToLoad2);
+            }
+            else if (index == 3)
+            {
+                SceneManager.LoadScene(levelToLoad3);
+            }
+            else if (index == 4)
+            {
+                SceneManager.LoadScene(levelToLoad4);
+            }
+        }
+    }
+}
